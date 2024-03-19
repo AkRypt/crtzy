440db267c97e03f2c335ff513c16e0aae3e63793
Akshit Dayal <akshitdayal99@gmail.com>
2024-03-19 18:57:42 -0400

    minor fixes

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -323,9 +323,15 @@ export default function Search() {
                             <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-orange-500 hover:bg-orange-600 focus:ring-4 focus:outline-none focus:ring-orange-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-orange-600 dark:hover:bg-orange-600 dark:focus:ring-orange-500">Search</button>
                         </div>
                     </form>
-                    <div className="flex justify-between items-center mb-2">
-                        <h2 className="text-lg font-bold">Search Results</h2>
-                    </div>
+                    {   !loading &&
+                        (influencers.length > 0 ? 
+                        <div className="flex justify-between items-center mb-2">
+                            <h2 className="text-lg font-bold">Search Results</h2>
+                        </div>
+                        :
+                        <h2 className="text-lg font-bold">We couldn't find anybody! Try broadening your search.</h2>
+                        )
+                    }
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                             {   // Influencer Card 1
                             influencers.map((influencer) => {
@@ -341,6 +347,7 @@ export default function Search() {
                                                 <div className="p-3">
                                                     <div className="flex justify-between -mb-2">
                                                         <h3 className="text-lg font-semibold">{influencer.name}</h3>
+                                                        {influencer.followers &&
                                                         <div className="flex ml-4 mt-1">
                                                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#494949" className="w-5 h-5">
                                                                 <path d="M4.5 6.375a4.125 4.125 0 1 1 8.25 0 4.125 4.125 0 0 1-8.25 0ZM14.25 8.625a3.375 3.375 0 1 1 6.75 0 3.375 3.375 0 0 1-6.75 0ZM1.5 19.125a7.125 7.125 0 0 1 14.25 0v.003l-.001.119a.75.75 0 0 1-.363.63 13.067 13.067 0 0 1-6.761 1.873c-2.472 0-4.786-.684-6.76-1.873a.75.75 0 0 1-.364-.63l-.001-.122ZM17.25 19.128l-.001.144a2.25 2.25 0 0 1-.233.96 10.088 10.088 0 0 0 5.06-1.01.75.75 0 0 0 .42-.643 4.875 4.875 0 0 0-6.957-4.611 8.586 8.586 0 0 1 1.71 5.157v.003Z" />
@@ -349,6 +356,7 @@ export default function Search() {
                                                                 {influencer.followers}
                                                             </p>
                                                         </div>
+                                                        }
                                                     </div>
                                                     <span className="text-sm font-normal mb-1">(@{influencer.username})</span>
                                                     <div className="">
